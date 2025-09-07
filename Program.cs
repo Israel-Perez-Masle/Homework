43ca5a0bf86b01f2e57a4f137fa178c152b6c1ef
BlackTitanX <205862776+Israel-Perez-Masle@users.noreply.github.com>
2025-09-07 17:35:32 -0400

    Added json support

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ using Microsoft.Extensions.Hosting;
 
 public class ChatHub : Hub
 {
-    public async Task SendMessage(string user, string message)
+    public async Task SendMessage(string message)
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        await Clients.All.SendAsync("ReceiveMessage", message);
     }
 }
 
--- obj/Debug/net9.0/ChatServer.AssemblyInfo.cs
+++ obj/Debug/net9.0/ChatServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0488baf9e977ba235dae580e1427c6de3e9f656")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b363cd7965e0097a250dedc6c32df87c979c895")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
