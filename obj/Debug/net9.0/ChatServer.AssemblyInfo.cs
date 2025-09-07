2702b1dfad8b875e628cea7c97a5fce02a366154
BlackTitanX <205862776+Israel-Perez-Masle@users.noreply.github.com>
2025-09-07 17:44:31 -0400

    Added support everywhere http

--- obj/Debug/net9.0/ChatServer.AssemblyInfo.cs
+++ obj/Debug/net9.0/ChatServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b363cd7965e0097a250dedc6c32df87c979c895")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43ca5a0bf86b01f2e57a4f137fa178c152b6c1ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
